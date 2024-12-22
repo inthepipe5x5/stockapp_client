d5c41c8ab294ea957cc87347f8d4efae645c32ef
lin_wsl <hao.lin0728@gmail.com>
2024-12-21 19:44:27 -0500

    update TypeScript configuration and Metro bundler setup; add nativewind-env.d.ts and adjust package dependencies - app runs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,6 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
-import { config } from "@/components/ui/gluestack-ui-provider/config";
 import StackNavigator from "@/navigation/StackNavigator";
 import ErrorWrapper from "@/components/errors/ErrorWrapper.jsx";
 import { globalErrorHandler } from "../lib/globalErrorHandler";
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
-import { getDefaultConfig } from 'expo/metro-config';
-import { withNativeWind } from 'nativewind/metro';
-  
+const { getDefaultConfig } = require('expo/metro-config'); // eslint-disable-line
+const { withNativeWind } = require('nativewind/metro'); //eslint-disable-line
+
 const config = getDefaultConfig(__dirname);
-  
-export default withNativeWind(config, { input: './global.css' });
+
+module.exports = withNativeWind(config, { input: './global.css' });
