86a81355976829a90961ec47c4fcdb9c18cfd1c6
lin_wsl <hao.lin0728@gmail.com>
2025-01-10 11:33:47 -0500

    moved (stacks) route into /app/(tabs) and added logic to redirect user to (auth) route if not authenticated

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,24 +1,68 @@
-import { Tabs } from "expo-router";
-import ProtectedNavigation from "../../components/navigation/ProtectedNavigation";
+import { Redirect, Tabs } from "expo-router";
+import ProtectedNavigation from "@/components/navigation/ProtectedNavigation";
+import { useUserSession } from "@/contexts/userSessionProvider";
+import { use, useEffect } from "react";
 /**
  * Root Tabs Navigator for authenticated users.
  *
  * Manages the top-level navigation between sections like Home, Dashboard, Search, and Profile.
  */
 function RootTabLayout() {
+  const { state } = useUserSession();
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    if (state && state.user) {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+  }, [state]);
+
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ tabBarLabel: "Home" }} />
       <Tabs.Screen
+        replace={() => {
+          auth ? <Redirect to="/(auth))" /> : null;
+        }}
+        name="home"
+        options={{ tabBarLabel: "Home" }}
+      />
+      <Tabs.Screen
+        replace={() => {
+          auth ? <Redirect to="/(auth))" /> : null;
+        }}
         name="(dashboard)/index"
         options={{ tabBarLabel: "Dashboard" }}
       />
-      <Tabs.Screen name="(search)/index" options={{ tabBarLabel: "Search" }} />
       <Tabs.Screen
+        replace={() => {
+          auth ? <Redirect to="/(auth))" /> : null;
+        }}
+        name="(search)/index"
+        options={{ tabBarLabel: "Search" }}
+      />
+      <Tabs.Screen
+        replace={() => {
+          auth ? <Redirect to="/(auth))" /> : null;
+        }}
         name="(profile)/index"
         options={{ tabBarLabel: "Profile" }}
       />
-      <Tabs.Screen name="/(inbox)/index" />
+      <Tabs.Screen
+        replace={() => {
+          auth ? <Redirect to="/(auth))" /> : null;
+        }}
+        name="/(inbox)"
+      />
+      <Tabs.Screen
+        name="(stacks)"
+        options={{
+          presentation: "transparentModal",
+          animation: "fade",
+          headerShown: false,
+        }}
+      />
     </Tabs>
   );
 }
